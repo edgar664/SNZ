c7efcb0af280e0438cee8e0eb2bb6dd0c0b88953
edgar664 <edgarbar18@gmail.com>
2026-03-11 16:30:27 -0600

    Fix1 base path GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
+  site: "https://edgar664.github.io",
+  base: "/SNZ",
+  build: {
+    format: "directory"
   }
-  
 });
